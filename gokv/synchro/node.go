44c997a944117552ab446d55c433733be946b0c8
jnnkrdb <jannek.rodenburger@icloud.com>
2025-06-08 20:19:04 +0000

    deactivated test-go workflow

--- gokv/synchro/node.go
+++ gokv/synchro/node.go
@@ -1,10 +1,17 @@
 package synchro
 
+import "fmt"
+
 var KnownHosts []Host
 
 type Host struct {
 	URI           string `json:"uri" yaml:"uri"`
 	GossipPortTCP int    `json:"gossip-port-tcp" yaml:"gossipPortTCP"`
+}
+
+// create string from Host
+func (h Host) String() string {
+	return fmt.Sprintf("%s:%d", h.URI, h.GossipPortTCP)
 }
 
 // this function reads the known hosts from a config file and
